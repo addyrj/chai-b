b88f012ef186676582eded44c9fe0b471714dc2c
aditya <addyrj20@gmail.com>
2024-03-13 13:40:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port  = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+app.get("/twitter", (req, res) => {
+  res.send("Welcome to Twitter");
+});
+app.get("/login", (req, res) => {
+  res.send("<h1>login </h1>");
+});
+app.listen(process.env.PORT, () => {
+  console.log(`listening on ${port}`);
+});
